6beafa1b7eccdf98631339b418968ecc30c9800e
jerhogan <jerhogan@hotmail.com>
2021-01-22 16:43:41 +0000

    Setup v1.0000.01 added.

--- BookListDB/Logger.cs
+++ BookListDB/Logger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,30 @@ namespace BookListDB
     public class Logger
     {
         private static readonly TraceSource _source = new TraceSource("JHDBBookListConsole");
+        private static string ReleaseLogFileName = "JHDBBookListRelease.Log";
+        public static void logToRelease(string logString)
+        {
+            if (!File.Exists(ReleaseLogFileName))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(ReleaseLogFileName))
+                {
+                    sw.WriteLine("Creating new Release Log FiLe.");
+                    sw.WriteLine("");
+                }
+            }
 
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            using (StreamWriter sw = File.AppendText(ReleaseLogFileName))
+            {
+                sw.WriteLine(logString);
+            }
+        }
         public static void OutputInformation(string message)
         {
-            Console.WriteLine(message); ;
+            Console.WriteLine(message);
+            logToRelease(message);
             _source.TraceEvent(TraceEventType.Information, 0, message);
             _source.Flush();
         }
@@ -21,12 +42,14 @@ namespace BookListDB
         {
             string fullMessage = string.Format(message, parameter);
             Console.WriteLine(fullMessage);
+            logToRelease(fullMessage);
             _source.TraceEvent(TraceEventType.Information, 0, message);
             _source.Flush();
         }
         public static void OutputError(string message)
         {
             Console.WriteLine(message);
+            logToRelease(message);
             _source.TraceEvent(TraceEventType.Error, 0, message);
             _source.Flush();
         }
@@ -34,6 +57,7 @@ namespace BookListDB
         {
             string fullMessage = string.Format(message, parameter);
             Console.WriteLine(fullMessage);
+            logToRelease(fullMessage);
             _source.TraceEvent(TraceEventType.Error, 0, fullMessage);
             _source.Flush();
         }
@@ -41,6 +65,7 @@ namespace BookListDB
         {
             string fullMessage = string.Format(message, intPar, strPar);
             Console.WriteLine(fullMessage);
+            logToRelease(fullMessage);
             _source.TraceEvent(TraceEventType.Error, 0, fullMessage);
             _source.Flush();
         }
